b44f729dfa4a0dc4cb8a84aa29eee7c23ac83647
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-08-28 16:52:30 -0600

    Fixed Aura Install

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#Install Flathub Packages
-flatpak install flathub com.obsproject.Studio com.valvesoftware.Steam net.davidotek.pupgui2 com.visualstudio.code com.discordapp.Discord flatpak install flathub net.lutris.Lutris com.usebottles.bottles -y
-flatpak install flathub us.zoom.Zoom org.onlyoffice.desktopeditors org.videolan.VLC -y
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 
 #Aura Install
 pacman -Sy git --noconfirm
@@ -19,10 +21,22 @@ echo "$aura_file"
 sudo pacman -U "$aura_file"
 
 #QEMU & KVM Install
+#Working
 pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat --noconfirm
 pacman -S ebtables iptables --noconfirm
 systemctl enable libvirtd.service
 sudo usermod -a -G libvirt $(whoami)
 systemctl restart libvirtd.service
+
+
+#Install Flatpak
+pacman -S flatpak
+
+#Install Flathub Packages
+flatpak install flathub com.obsproject.Studio com.valvesoftware.Steam net.davidotek.pupgui2 com.visualstudio.code com.discordapp.Discord flatpak install flathub net.lutris.Lutris com.usebottles.bottles -y
+flatpak install flathub us.zoom.Zoom org.onlyoffice.desktopeditors org.videolan.VLC -y
+
+#Install Github Desktop
+aura -A github-desktop
 
 echo Done
