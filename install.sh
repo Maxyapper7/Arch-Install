a0af0619b0ca7fddf6342dc10085b6eeb40db977
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-08-28 17:14:09 -0600

    Aura Fix 3

--- install.sh
+++ install.sh
@@ -7,7 +7,19 @@ fi
 
 
 #Aura Install
-./aura-install.sh
+#!/bin/bash
+pacman -Sy git --noconfirm
+git clone https://aur.archlinux.org/aura-bin.git
+cd aura-bin
+ su su - "$USER" -c makepkg -s
+
+aura_file=$(find . -type f -name "aura-*")
+
+aura_file="${aura_file#??}"
+
+echo "$aura_file"
+
+sudo pacman -U "$aura_file"
 
 #QEMU & KVM Install
 #Working
