98cf9e1c482f728d02f4dac700c3a9a24b7edb78
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-08-28 17:01:23 -0600

    Aura Fix 2

--- install.sh
+++ install.sh
@@ -7,30 +7,19 @@ fi
 
 
 #Aura Install
-pacman -Sy git --noconfirm
-git clone https://aur.archlinux.org/aura-bin.git
-cd aura-bin
-makepkg -s
-
-aura_file=$(find . -type f -name "aura-*")
-
-aura_file="${aura_file#??}"
-
-echo "$aura_file"
-
-sudo pacman -U "$aura_file"
+./aura-install.sh
 
 #QEMU & KVM Install
 #Working
 pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat --noconfirm
 pacman -S ebtables iptables --noconfirm
 systemctl enable libvirtd.service
-sudo usermod -a -G libvirt $(whoami)
+usermod -a -G libvirt $(whoami) --noconfirm
 systemctl restart libvirtd.service
 
 
 #Install Flatpak
-pacman -S flatpak
+pacman -S flatpak --noconfirm
 
 #Install Flathub Packages
 flatpak install flathub com.obsproject.Studio com.valvesoftware.Steam net.davidotek.pupgui2 com.visualstudio.code com.discordapp.Discord flatpak install flathub net.lutris.Lutris com.usebottles.bottles -y
